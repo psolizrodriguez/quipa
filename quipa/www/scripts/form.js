67c1b1979611d91f43100531c61bf77878a1943b
catemlitten <clitten@luc.edu>
2017-09-17 18:47:33 -0500

    initial commit. right now a very basic single page with a form that alerts you what you have put in

---
+++ quipa/www/scripts/form.js
@@ -0,0 +1,24 @@
+﻿/*
+function testFunction() {
+    alert("Test function is working...");
+}
+
+$(document).ready(testFunction);
+*/
+
+$(document).ready(function () {
+    $("input[type=submit]").click(function (e) {
+        var firstName = $("#firstName").val();
+        var lastName = $("#surname").val();
+        var email = $("#signUpEmail").val();
+        var age = $("#age").val();
+        
+        if (firstName == '' || lastName == '' || email == '') {
+            e.preventDefault();
+            alert("Please Fill Required Fields");
+        }
+
+        alert("You are " + firstName + " " + lastName + ", " + age + " years old and we can contact you at\n" + email);
+
+    });
+});
