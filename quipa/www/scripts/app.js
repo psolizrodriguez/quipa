ba6df2c22a54a7be8cf47eb57c6dbb91d07ed908
catemlitten <clitten@luc.edu>
2017-10-12 19:58:58 -0500

    Map working, changing things over. Need to clean up files. Angular is a beast to try to learn fast.

---
+++ quipa/www/scripts/app.js
@@ -0,0 +1,10 @@
+﻿
+(function () {
+    'use strict';
+    var module = angular.module('app', ['onsen']);
+
+    module.controller('AppController', function ($scope) {
+
+    });
+
+})();
--- quipa/www/scripts/plugins.js
+++ quipa/www/scripts/plugins.js
@@ -1,67 +1,58 @@
-﻿(function () {
+﻿
 
-    //cordova - add listener to DOM & check deviceready event
-    document.addEventListener('deviceready', function (event) {
-        //prevent any bound defaults
-        event.preventDefault();
-        console.log("cordova checked...device ready");
+document.addEventListener('init', function (event) {
+    var page = event.target;
 
-        //onsen - init event is fired after ons-page attached to DOM...
-        document.addEventListener('init', onsInit, false);
+    if (page.id === 'login') {
+        page.querySelector('#push-button').onclick = function () {
+            document.querySelector('#Navigator').pushPage('register.html', { data: { title: 'Register' } });
+        };
+    } else if (page.id === 'register') {
+        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+        var page = event.target.id;
+        getLocation();
+    }
 
-        function onsInit(e) {
-            var page = e.target.id;
-            console.log("page-event=" + page);
-            getLocation();
-        }
+    function onSuccess(location) {
+        //location recording time
+        var locationTime = Date(location.timestamp);
+        //device's latitude
+        var lat = location.coords.latitude;
+        //device's longitude
+        var long = location.coords.longitude;
+        //device's heading - depends on device specs and support
+        var myHeading = location.coords.heading;
+        //output result to #location div...
+        var locationDiv = document.getElementById("location");
+        locationDiv.innerHTML = "<p>my latitude = " + lat + "</p><p>my longitude = " + long + "</p>";
+        //build map with current latitude and longitude
+        buildMap(lat, long);
+    }
 
-        function onSuccess(location) {
-            //location recording time
-            var locationTime = Date(location.timestamp);
-            //device's latitude
-            var myLatitude = location.coords.latitude;
-            //device's longitude
-            var myLongitude = location.coords.longitude;
-            //device's altitude - depends on device specs and support
-            var myAltitude = location.coords.altitude;
-            //device's heading - depends on device specs and support
-            var myHeading = location.coords.heading;
-            //output test to console...
-            console.log("found location successfully!");
-            //output result to #location div...
-            var locationDiv = document.getElementById("location");
-            locationDiv.innerHTML = "<p>my latitude = " + myLatitude + "</p><p>my longitude = " + myLongitude + "</p>";
-            //build map with current latitude and longitude
-            buildMap(myLatitude, myLongitude);
-        }
+    function onFail(error) {
+        document.getElementById("location").innerHTML = "location error code = " + error.code + " message = " + error.message;
+    }
 
-        function onFail(error) {
-            document.getElementById("location").innerHTML = "location error code = " + error.code + " message = " + error.message;
-        }
+    function getLocation() {
+        navigator.geolocation.getCurrentPosition(onSuccess,
+            onFail, {
+                timeout: 15000,
+                enableHighAccuracy: true
+            });
+    }
 
-        function getLocation() {
-            navigator.geolocation.getCurrentPosition(onSuccess,
-                onFail, {
-                    timeout: 15000,
-                    enableHighAccuracy: true
-                });
-        }
-
-        function buildMap(lat, long) {
-            //set combined position for user
-            var latlong = new google.maps.LatLng(lat, long);
-            //set required options
-            var mapOptions = {
-                center: latlong,
-                zoom: 12,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            //build map in map div...
-            var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-            //add initial location marker
-            var marker = new google.maps.Marker({ position: latlong, map: map });
-        }
-
-    }, false);
-
-})();
+    function buildMap(lat, long) {
+        //set combined position for user
+        var latlong = new google.maps.LatLng(lat, long);
+        //set required options
+        var mapOptions = {
+            center: latlong,
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        //build map in map div...
+        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        //add initial location marker
+        var marker = new google.maps.Marker({ position: latlong, map: map });
+    }
+    });
