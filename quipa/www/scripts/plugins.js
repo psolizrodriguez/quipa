aa68faf300336ccf5ea75cdc8778a92efe44fb19
Pinky Sindhu <pinky@Pinkys-MacBook-Pro.local>
2017-10-19 18:09:52 -0500

    - Adding Profile Screen - Adding Profile Hire - Adding Request List Screen

--- quipa/www/scripts/plugins.js
+++ quipa/www/scripts/plugins.js
@@ -1,4 +1,4 @@
-﻿
+
 
 document.addEventListener('init', function (event) {
     var page = event.target;
@@ -47,6 +47,34 @@ document.addEventListener('init', function (event) {
         
 
     }
+    /*Pinky*/
+    else if (page.id === 'profileHire') {
+         
+      var modal = document.querySelector('ons-modal');
+      modal.show();
+        
+    }
+    else if (page.id === 'profileSend') {
+
+        var date = new Date();
+
+        var currentHours = date.getHours();
+        currentHours = ("0" + currentHours).slice(-2);
+
+        var currentMinutes = date.getMinutes();
+        currentMinutes = ("0" + currentMinutes).slice(-2);
+
+        $('#fromTime').val(currentHours + ':' + currentMinutes);
+        $('#toTime').val(currentHours + ':' + currentMinutes);
+
+    }else if (page.id == "requestHired") {
+    
+          var modal = document.querySelector('ons-modal');
+          modal.show();
+                          
+     }
+      
+    /*Pinky*/
 
     //SKILL TOGGLE FOR SEARCH PAGE
     function toggleDrop() {
@@ -206,3 +234,159 @@ document.addEventListener('init', function (event) {
 
 
 });
+
+document.addEventListener('show', function(event) {
+  var page = event.target;
+  
+  if (page.id == "requestHired") {
+      
+      var modal = document.querySelector('ons-modal');
+      modal.show();
+                          
+      var profileId = 1;
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://18.220.231.8:8080/QuipaServer/services/requestservice/request?profileId="+profileId);
+      xhr.setRequestHeader("Accept", "application/json");
+      
+      xhr.onload = function() {
+                          
+          modal.hide();
+          try {
+              if (this.status === 200) {
+                  
+                  var data = JSON.parse(this.response);
+                  console.log(JSON.stringify(data));
+                          
+                          
+                  var element = document.getElementById("myrequestContent");
+                  element.innerHTML = '';
+                          
+                  for(var i=0; i< data.length; i++) {
+                          
+                      var currentItem = data[i];
+                      var statusColor = "green";
+                      if(currentItem['status'].toLowerCase() === 'pending') {
+                          statusColor = "orange";
+                      }
+                          
+                      element.appendChild(ons.createElement(
+                                                            '<ons-list-item>' +
+                                                            '<div class="center">' +
+                                                            '<div style="width:100%;">' +
+                                                            '<span style="float:left;line-height: 50px;font-size:18px;font-weight:bold;">Kelly Blackwell</span>' +
+                                                            '</div>' +
+                                                            '<div style="width:100%;">' +
+                                                            '<img src="http://placekitten.com/g/30/30" style="float:left;">' +
+                                                            '<span style="float:left;margin-left:20px;line-height: 30px;font-size:18px;font-weight:bold;">for '+ currentItem['hours'] +' hours</span>' +
+                                                            '</div>' +
+                                                            '<div  style="width:100%;height:40px;">' +
+                                                            '<span style="float:left;font-size:20px;line-height: 40px;">on </span>' +
+                                                            '<span style="float:right;padding-right:20px;font-size:18px;line-height: 40px;font-weight:bold;">10-19-2017</span>' +
+                                                            '</div><br/><br/>' +
+                                                            '<div  style="width:100%;height:30px;">' +
+                                                            '<span style="float:left;font-size:20px;line-height: 30px;">from</span>' +
+                                                            '<span style="float:right;font-weight:bold;font-size:18px;padding-right:20px;">'+ currentItem['fromHour'] +'</span>' +
+                                                            '</div><br/><br/>' +
+                                                            '<div  style="width:100%;height:30px;">' +
+                                                            '<span style="float:left;font-size:20px;line-height: 30px;">to</span>' +
+                                                            '<span style="float:right;font-weight:bold;font-size:18px;padding-right:20px;">'+ currentItem['toHour'] +'</span>' +
+                                                            '</div><br/><br/>' +
+                                                            '<div  style="width:100%;height:30px;">' +
+                                                            '<span style="float:left;font-size:20px;line-height: 30px;">Total</span>' +
+                                                            '<span style="float:right;font-weight:bold;font-size:18px;padding-right:20px;">$'+ currentItem['total'] +'</span>' +
+                                                            '</div>' +
+                                                            '</div>' +
+                                                            '<div class="right">' +
+                                                            '<div style="float:left;width:120px;">' +
+                                                            '<img src="http://placekitten.com/g/120/120" />' +
+                                                            '<br clear="all"/><br clear="all"/>' +
+                                                            '<div style="float:left;width:100%;">' +
+                                                            '<ons-button onclick="" modifier="large" style="background:' + statusColor +';">'+ currentItem['status'] +'</ons-button>' +
+                                                            '</div>' +
+                                                            '</div>' +
+                                                            '</div>' +
+                                                            '</ons-list-item>'
+                                                            ));
+                          
+                  }
+              }
+          } catch (e) {
+              console.log(e.message);
+          }
+      };
+      xhr.onerror = function() {
+          modal.hide();
+          console.log(this.status + " " + this.statusText);
+      };
+      xhr.send();
+                          
+  }else if (page.id === 'profileHire') {
+                      
+      var modal = document.querySelector('ons-modal');
+      modal.show();
+                          
+      var profileId = 37;
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://18.220.231.8:8080/QuipaServer/services/profileservice/profile/"+profileId);
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.onload = function() {
+                          
+          modal.hide();
+          try {
+              if (this.status === 200) {
+                  $('#star').raty({ 'score': 4, 'readonly': true });
+                          
+                  numReviews = 20;
+                  var data = JSON.parse(this.response);
+                  document.getElementById('numReviews').innerHTML = numReviews + " Reviews";
+                  document.getElementById('profileName').innerHTML = data.name;
+                  document.getElementById('profileDescription').innerHTML = data.description;
+                  var profileImage = document.getElementById('profileImage');
+                  profileImage.setAttribute('src', data.profilePicture);
+                  var parentDiv = document.getElementById('thumbs');
+                  
+                  var hourlyRate = document.getElementById('hourlyRate');
+                  hourlyRate.innerHTML =  "$" + data.priceHour;
+                  hourlyRate.setAttribute('style', 'text-align:left;font-size:5;font-weight:bold');
+      
+                  var obj = {
+                              1: "images/computer.png",
+                              2: "images/cooker.png",
+                              3: "images/hair_stylist.png",
+                              4: "images/handyman.png",
+                              5: "images/mechanic.png",
+                              6: "images/painter.png",
+                              7: "images/telephonist.png",
+                              8: "images/tutor.png",
+                              9: "images/waitress.png"
+      
+                              };
+                  var skillnum = data.skills.length/3;
+                  if (skillnum !=0){
+                      var num = 1;
+                      for(i=1; i<=skillnum; i++){
+                          var ahref = document.createElement("a");
+                          ahref.setAttribute('href', '#');
+                          var img = document.createElement("img");
+                          img.setAttribute('src', obj[data.skills[num]]);
+                          img.setAttribute('style', 'margin-left:10px;');
+                          num = num+3;
+                          ahref.appendChild(img);
+                          parentDiv.appendChild(ahref);
+                      }
+                  }
+              }
+      
+          } catch (e) {
+              console.log(e.message);
+          }
+      };
+      xhr.onerror = function() {
+          modal.hide();
+          console.log(this.status + " " + this.statusText);
+      };
+      xhr.send();
+  }
+});
+
